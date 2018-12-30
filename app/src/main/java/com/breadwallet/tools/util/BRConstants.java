4ba692c60ec7b348ff49b5060fe4cb8892ddcaab
Pie <pie@netabuse.net>
2018-12-30 21:09:36 +0100

    Nixification

--- app/src/main/java/com/breadwallet/tools/util/BRConstants.java
+++ app/src/main/java/com/breadwallet/tools/util/BRConstants.java
@@ -68,7 +68,7 @@ public class BRConstants {
     public static final String FEE_KB_PREFS = "feeKb";
     public static final String ECONOMY_FEE_KB_PREFS = "EconomyFeeKb";
     public static final String LITTLE_CIRCLE = "\u2022";
-    public static String SUPPORT_EMAIL = "contact@nixplatform.io";
+    public static String SUPPORT_EMAIL = "development@nixplatform.io";
 
     public static final int ONE_BITCOIN = 100000000;
     public static final int HUNDRED_BITS = 10000000;
